4218f31b8ffdbc37919275ca89aa22f297581789
Matthew LaRocca <mlarocca.cpp@gmail.com>
2019-08-12 15:51:05 -0700

    Trait Fixtures

--- test/test_traits.c
+++ test/test_traits.c
@@ -6,11 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void trait_after_each() {
+void trait_test_setup() {}
+
+void trait_test_teardown() {
 	cutil_trait_destroy();
 }
 
-void trait_int_compare() {
+CTEST_SUITE(trait, trait_test_setup, trait_test_teardown)
+
+void int_compare() {
 	cutil_trait* int_trait = cutil_trait_int();
 	int a = -1, b1 = 1, b2 = 1, c = 2;
 
@@ -19,12 +23,12 @@ void trait_int_compare() {
 	CTEST_ASSERT_INT_GT(int_trait->compare_func(&c, &a, int_trait->user_data), 0);
 }
 
-void trait_int_size() {
+void int_size() {
 	cutil_trait* int_trait = cutil_trait_int();
 	CTEST_ASSERT_INT_EQ(int_trait->size, sizeof(int));
 }
 
-void trait_uint_compare() {
+void uint_compare() {
 	cutil_trait* uint_trait = cutil_trait_uint();
 	unsigned int a1 = 1, a2 = 1, b = 2;
 
@@ -33,12 +37,12 @@ void trait_uint_compare() {
 	CTEST_ASSERT_INT_GT(uint_trait->compare_func(&b, &a1, uint_trait->user_data), 0);
 }
 
-void trait_float_size() {
+void float_size() {
 	cutil_trait* float_trait = cutil_trait_float();
 	CTEST_ASSERT_INT_EQ(float_trait->size, sizeof(float));
 }
 
-void trait_float_compare() {
+void float_compare() {
 	cutil_trait* float_trait = cutil_trait_int();
 	float a = -1.0f, b1 = 1.0f, b2 = 1.0f, c = 2.0f;
 
@@ -47,12 +51,12 @@ void trait_float_compare() {
 	CTEST_ASSERT_INT_GT(float_trait->compare_func(&c, &a, float_trait->user_data), 0);
 }
 
-void trait_uint_size() {
+void uint_size() {
 	cutil_trait* uint_trait = cutil_trait_uint();
 	CTEST_ASSERT_INT_EQ(uint_trait->size, sizeof(unsigned int));
 }
 
-void trait_ptr_compare() {
+void ptr_compare() {
 	cutil_trait* ptr_trait = cutil_trait_ptr();
 	int* test = calloc(10, sizeof(int));
 
@@ -66,17 +70,17 @@ void trait_ptr_compare() {
 	free(test);
 }
 
-void trait_ptr_size() {
+void ptr_size() {
 	cutil_trait* ptr_trait = cutil_trait_ptr();
 	CTEST_ASSERT_INT_EQ(ptr_trait->size, sizeof(void*));
 }
 
-void trait_cstring_size() {
+void cstring_size() {
 	cutil_trait* cstring_trait = cutil_trait_cstring();
 	CTEST_ASSERT_INT_EQ(cstring_trait->size, sizeof(char**));
 }
 
-void trait_cstring_copy() {
+void cstring_copy() {
 	cutil_trait* cstring_trait = cutil_trait_cstring();
 
 	char* expected_string = "hello world!";
@@ -89,7 +93,7 @@ void trait_cstring_copy() {
 	free(str_ptr);
 }
 
-void trait_cstring_compare() {
+void cstring_compare() {
 	cutil_trait* cstring_trait = cutil_trait_cstring();
 	char* str1 = "hello";
 	char* str2 = "world";
@@ -99,69 +103,62 @@ void trait_cstring_compare() {
 	CTEST_ASSERT_INT_GT(cstring_trait->compare_func(&str2, &str1, cstring_trait->user_data), 0);
 }
 
-void default_trait_int() {
+void default_int() {
 	cutil_trait* int_trait1 = cutil_trait_int();
 	cutil_trait* int_trait2 = cutil_trait_int();
 
 	CTEST_ASSERT_PTR_EQ(int_trait1, int_trait2);
 }
 
-void default_trait_uint() {
+void default_uint() {
 	cutil_trait* uint_trait1 = cutil_trait_uint();
 	cutil_trait* uint_trait2 = cutil_trait_uint();
 
 	CTEST_ASSERT_PTR_EQ(uint_trait1, uint_trait2);
 }
 
-void default_trait_float() {
+void default_float() {
 	cutil_trait* float_trait1 = cutil_trait_float();
 	cutil_trait* float_trait2 = cutil_trait_float();
 
 	CTEST_ASSERT_PTR_EQ(float_trait1, float_trait2);
 }
 
-void default_trait_ptr() {
+void default_ptr() {
 	cutil_trait* ptr_trait1 = cutil_trait_ptr();
 	cutil_trait* ptr_trait2 = cutil_trait_ptr();
 
 	CTEST_ASSERT_PTR_EQ(ptr_trait1, ptr_trait2);
 }
 
-void default_trait_cstring() {
+void default_cstring() {
 	cutil_trait* cstring_trait1 = cutil_trait_cstring();
 	cutil_trait* cstring_trait2 = cutil_trait_cstring();
 
 	CTEST_ASSERT_PTR_EQ(cstring_trait1, cstring_trait2);
 }
 
-void default_trait_after_each() {
-	cutil_trait_destroy();
-}
-
 void add_trait_tests() {
-    ctest_suite("traits");
-    ctest_suite_after_each(&trait_after_each);
+    CTEST_ADD_TEST_S(trait, int_compare);
+    CTEST_ADD_TEST_S(trait, int_size);
 
-    CTEST_ADD_TEST(trait_int_compare);
-    CTEST_ADD_TEST(trait_int_size);
+	CTEST_ADD_TEST_S(trait, uint_compare);
+	CTEST_ADD_TEST_S(trait, uint_size);
 
-	CTEST_ADD_TEST(trait_uint_compare);
-	CTEST_ADD_TEST(trait_uint_size);
+	CTEST_ADD_TEST_S(trait, float_compare);
+	CTEST_ADD_TEST_S(trait, float_size);
 
-	CTEST_ADD_TEST(trait_float_compare);
-	CTEST_ADD_TEST(trait_float_size);
+    CTEST_ADD_TEST_S(trait, ptr_compare);
+    CTEST_ADD_TEST_S(trait, ptr_size);
 
-    CTEST_ADD_TEST(trait_ptr_compare);
-    CTEST_ADD_TEST(trait_ptr_size);
-
-    CTEST_ADD_TEST(trait_cstring_compare);
-    CTEST_ADD_TEST(trait_cstring_copy);
-    CTEST_ADD_TEST(trait_cstring_size);
+    CTEST_ADD_TEST_S(trait, cstring_compare);
+    CTEST_ADD_TEST_S(trait, cstring_copy);
+    CTEST_ADD_TEST_S(trait, cstring_size);
 	/* TODO: Test cstring delete */
 
-    CTEST_ADD_TEST(default_trait_int);
-	CTEST_ADD_TEST(default_trait_uint);
-	CTEST_ADD_TEST(default_trait_float);
-    CTEST_ADD_TEST(default_trait_ptr);
-    CTEST_ADD_TEST(default_trait_cstring);
+    CTEST_ADD_TEST_S(trait, default_int);
+    CTEST_ADD_TEST_S(trait, default_uint);
+    CTEST_ADD_TEST_S(trait, default_float);
+    CTEST_ADD_TEST_S(trait, default_ptr);
+    CTEST_ADD_TEST_S(trait, default_cstring);
 }
