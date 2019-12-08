bf0e80d32dc5bd11c6fc4529fe5c205579facdb3
Matthew LaRocca <mlarocca.cpp@gmail.com>
2019-12-08 10:18:00 -0800

    Add allocator struct

---
+++ src/allocator_private.h
@@ -0,0 +1,9 @@
+#ifndef CUTIL_ALLOCATOR_PRIVATE_H
+#define CUTIL_ALLOCATOR_PRIVATE_H
+
+#include "cutil/allocator.h"
+
+cutil_allocator* cutil_get_defult_allocator();
+cutil_allocator* cutil_get_current_allocator();
+
+#endif
