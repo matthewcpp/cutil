971926b23d9ecb94114d4000bcb4a972277a88d3
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-01-07 20:10:55 -0800

    add forward_list tests

--- test/test_main.c
+++ test/test_main.c
@@ -17,6 +17,8 @@ int main(int argc, char** argv) {
 
     add_vector_tests();
     add_list_tests();
+    add_forward_list_tests();
+    add_forward_list_itr_tests();
     add_list_itr_tests();
     add_btree_tests();
     add_btree_itr_tests();
--- test/test_suites.h
+++ test/test_suites.h
@@ -3,6 +3,8 @@
 
 void add_vector_tests();
 void add_list_tests();
+void add_forward_list_tests();
+void add_forward_list_itr_tests();
 void add_list_itr_tests();
 void add_btree_tests();
 void add_btree_itr_tests();
